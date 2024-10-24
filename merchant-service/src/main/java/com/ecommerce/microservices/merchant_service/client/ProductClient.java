b1f3e98d5ba40940549f06f65f94591814aa1294
Shreyas <shreyaskapse171@gmail.com>
2024-10-24 13:37:52 +0530

    linked add product api

--- merchant-service/src/main/java/com/ecommerce/microservices/merchant_service/client/ProductClient.java
+++ merchant-service/src/main/java/com/ecommerce/microservices/merchant_service/client/ProductClient.java
@@ -1,10 +1,17 @@
 package com.ecommerce.microservices.merchant_service.client;
 
+import com.ecommerce.microservices.merchant_service.dto.ProductDTO;
+import com.ecommerce.microservices.merchant_service.utils.DefaultResponse;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
 @HttpExchange
 public interface ProductClient {
     @GetExchange("/product/test")
     public String productTest();
+
+    @PostExchange("/product/add-product")
+    public DefaultResponse addProduct(@RequestBody ProductDTO productDTO);
 }
