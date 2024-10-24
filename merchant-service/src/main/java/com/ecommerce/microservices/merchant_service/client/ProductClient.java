197e7c5b449a659b80b4cc648fc7ac8b0c7c5505
Shreyas <shreyaskapse171@gmail.com>
2024-10-25 00:26:48 +0530

    added connection between products and merchant to get all products of merchants with pagination

--- merchant-service/src/main/java/com/ecommerce/microservices/merchant_service/client/ProductClient.java
+++ merchant-service/src/main/java/com/ecommerce/microservices/merchant_service/client/ProductClient.java
@@ -2,7 +2,11 @@ package com.ecommerce.microservices.merchant_service.client;
 
 import com.ecommerce.microservices.merchant_service.dto.ProductDTO;
 import com.ecommerce.microservices.merchant_service.utils.DefaultResponse;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
@@ -14,4 +18,11 @@ public interface ProductClient {
 
     @PostExchange("/product/add-product")
     public DefaultResponse addProduct(@RequestBody ProductDTO productDTO);
+
+    @GetExchange("/product/all/{merchantId}")
+    public ResponseEntity<DefaultResponse> getProductsOfMerchantByMerchantId(
+            @Parameter(description = "Merchant Id") @PathVariable("merchantId") String merchantId,
+            @RequestParam int page,
+            @RequestParam int size
+    );
 }
