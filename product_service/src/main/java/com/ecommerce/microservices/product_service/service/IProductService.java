a4b5f0ed06e128b836fce6d59af0589dc24031a1
Shreyas <shreyaskapse171@gmail.com>
2024-10-25 00:24:39 +0530

    updated return type

--- product_service/src/main/java/com/ecommerce/microservices/product_service/service/IProductService.java
+++ product_service/src/main/java/com/ecommerce/microservices/product_service/service/IProductService.java
@@ -1,14 +1,12 @@
 package com.ecommerce.microservices.product_service.service;
 
 import com.ecommerce.microservices.product_service.dto.ProductDTO;
-import com.ecommerce.microservices.product_service.entity.ProductEntity;
 import com.ecommerce.microservices.product_service.utils.DefaultResponse;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
 
 public interface IProductService {
 
     DefaultResponse addProduct(@Valid ProductDTO productEntity);
 
-    Page<ProductDTO> getProductsOfMerchantByMerchantId(String merchantId, int page, int size);
+    DefaultResponse getProductsOfMerchantByMerchantId(String merchantId, int offset, int limit);
 }
