ad3d5b9a0ad21241e408f23f5a7b89ca69b760b9
Shreyas <shreyaskapse171@gmail.com>
2024-10-24 12:38:20 +0530

    added default response

---
+++ product_service/src/main/java/com/ecommerce/microservices/product_service/utils/DefaultResponse.java
@@ -0,0 +1,29 @@
+package com.ecommerce.microservices.product_service.utils;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class DefaultResponse {
+    private boolean success;
+    private String message;
+
+    @Builder.Default
+    private Optional<Map<String, String>> errors = Optional.empty();
+
+    @Builder.Default
+    private Optional<Map<String, String>> data = Optional.empty();
+
+    private Optional<HttpStatus> httpStatus;
+}
